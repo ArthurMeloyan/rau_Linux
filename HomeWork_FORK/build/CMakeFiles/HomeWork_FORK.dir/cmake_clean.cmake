31f31f9f84e9c6e0c357e6c7c1d93cb6e2c564ea
Arthur Meloyan <mel@A-Mel.Dlink>
2022-12-01 01:03:26 +0400

    fork

---
+++ HomeWork_FORK/build/CMakeFiles/HomeWork_FORK.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HomeWork_FORK.dir/main.c.o"
+  "CMakeFiles/HomeWork_FORK.dir/main.c.o.d"
+  "HomeWork_FORK"
+  "HomeWork_FORK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HomeWork_FORK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
