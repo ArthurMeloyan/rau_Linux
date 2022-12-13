f50efecd54ae09cbeda275887202eb7631539b6c
ArthurMeloyan <amel@Arthur>
2022-12-13 16:44:34 +0400

    && and ||

---
+++ HomeWork_6/AND/build/CMakeFiles/and.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/and.dir/and.c.o"
+  "CMakeFiles/and.dir/and.c.o.d"
+  "and"
+  "and.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/and.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HomeWork_6/OR/or.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char** argv)
+{
+    if (argc == 1)
+    {
+        printf("No program name is provided\n");
+        return 1;
+    }
+    int status;
+
+    for (int i = 1; i < argc; ++i)
+    {
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            printf("Unable to fork\n");
+            return 1;
+        }
+
+        
+        if (pid == 0)
+        {
+            char* child_args[] = {argv[i], NULL};
+            if (execvp(argv[i], child_args) == -1)
+            {
+                printf("%s: %s\n",argv[i], "Command not found");
+                exit(1);
+            }
+            else
+            {
+                exit(0);
+            }
+        }
+        wait(&status);
+        if (status == 0)
+            break;
+
+    }
+}
