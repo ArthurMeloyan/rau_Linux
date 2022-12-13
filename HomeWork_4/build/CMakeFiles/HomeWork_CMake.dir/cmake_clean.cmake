edd7fd69b3a9970fef46bc539f0e6c2551e6d5ff
ArthurMeloyan <amel@Arthur>
2022-12-13 16:43:16 +0400

    CMake

---
+++ HomeWork_4/build/CMakeFiles/HomeWork_CMake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HomeWork_CMake.dir/main.cc.o"
+  "CMakeFiles/HomeWork_CMake.dir/src/Polynomial.cc.o"
+  "HomeWork_CMake"
+  "HomeWork_CMake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HomeWork_CMake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
