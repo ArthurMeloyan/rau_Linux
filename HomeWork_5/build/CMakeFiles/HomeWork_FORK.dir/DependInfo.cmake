5403b4d574d77f3c365dfef1b7b24f6ec96a83e0
ArthurMeloyan <amel@Arthur>
2022-12-13 16:44:03 +0400

    Child process

---
+++ HomeWork_5/build/CMakeFiles/HomeWork_FORK.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mel/Рабочий стол/HW/rau_Linux/HomeWork_FORK/main.c" "CMakeFiles/HomeWork_FORK.dir/main.c.o" "gcc" "CMakeFiles/HomeWork_FORK.dir/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ HomeWork_5/build/CMakeFiles/HomeWork_FORK.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for HomeWork_FORK.
+# This may be replaced when dependencies are built.
